0291407aa2b597b212dbdc06b0355d064606fdb2
HappyRa1n <HappyRa1n@yandex.ru>
2020-12-08 12:39:52 +0500

    Add BinarySearch_Inserting_Sort and Cocktail_Sort

---
+++ BinarySearch_Insertion_Sort/BinarySearch_Sort.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+void Binary_sort(int* mas, int n) {
+	int l;
+	int r;
+	for (int i = 1; i < n; i++) {
+		int c = mas[i];
+		r = i;
+		l = 0;
+		while (l < r) {
+			int m = (l + r) / 2;
+			if (mas[m] <= c)
+				l = m + 1;
+			else
+				r = m;
+		}
+		for (int j = i; j > l; j--) {
+			mas[j] = mas[j - 1];
+		}
+		mas[l] = c;
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	int* mas = new int[n];
+
+
+	for (int i = 0; i < n; i++)
+		cin >> mas[i];
+	Binary_sort(mas, n);
+	for (int i = 0; i < n; i++) {
+		cout << mas[i] << " ";
+	}
+}
---
+++ Cocktail_sort/Cocktail_sort.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+void sort_sheiker(int mas[], int n) {
+	int left = 0;
+	int right = n - 1;
+	int b = true;
+	while (b&& left < right) {
+		b = false;
+		for (int i = left; i < right; i++)
+		{
+			if (mas[i] > mas[i + 1])
+			{
+				swap(mas[i], mas[i + 1]);
+				b = true;
+			}
+		}
+		right--;
+		for (int j = right; j > left; j--) {
+			if (mas[j] < mas[j - 1])
+			{
+				swap(mas[j], mas[j - 1]);
+				b = true;
+			}
+		}
+		left++;
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	int* mas = new int[n];
+	for (int i = 0; i < n; i++)
+		cin >> mas[i];
+	sort_sheiker(mas, n);
+	cout << endl;
+	for (int i = 0; i < n; i++) {
+		cout << mas[i] << " ";
+	}
+}
